a930d00f72e9907ce9277bf7fad737c558ca7f73
Kauã Maximino <kaua_maximino@outlook.com>
2022-04-26 18:15:42 -0300

    requisito 5 ok

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,46 @@
-import React from 'react';
-// import AppContext from '../contexts/AppContext';
+import React, { useContext } from 'react';
+import AppContext from '../contexts/AppContext';
 
-const Login = () => (
-  <div>
-    <input
-      type="email"
-      placeholder="Email"
-      data-testid="email-input"
-    />
+const Login = () => {
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+  } = useContext(AppContext);
 
-    <input
-      type="password"
-      placeholder="Password"
-      data-testid="password-input"
-    />
+  const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  const six = 6;
 
-    <button
-      type="button"
-      data-testid="login-submit-btn"
-    >
-      Enter
-    </button>
-  </div>
-);
+  return (
+    <div>
+      <input
+        type="email"
+        placeholder="Email"
+        data-testid="email-input"
+        value={ email }
+        onChange={ ({ target: { value } }) => setEmail(value) }
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        data-testid="password-input"
+        value={ password }
+        onChange={ (e) => {
+          setPassword(e.target.value);
+        } }
+      />
+
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ !(regex.test(email) && password.length > six) }
+      >
+        Enter
+      </button>
+    </div>
+  );
+};
 
 export default Login;
--- src/contexts/AppProvider.jsx
+++ src/contexts/AppProvider.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import AppContext from './AppContext';
 
 function AppProvider({ children }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const data = { email, setEmail, password, setPassword };
+
   return (
-    <AppContext.Provider value={ {} }>
+    <AppContext.Provider value={ data }>
       {children}
     </AppContext.Provider>
   );
