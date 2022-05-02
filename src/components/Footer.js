a4082f985b9920a3126e4eb13df24912e6ea8a52
Kauã Maximino <kaua_maximino@outlook.com>
2022-05-02 16:30:17 -0300

    requisito 20 ok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import DrinkIcon from '../images/drinkIcon.svg';
+import ExploreIcon from '../images/exploreIcon.svg';
+import MealIcon from '../images/mealIcon.svg';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div
+    <footer
       className="footer"
       data-testid="footer"
     >
       <Link
         to="/drinks"
         data-testid="drinks-bottom-btn"
+        src={ DrinkIcon }
       >
-        Bebidas
+        <img src={ DrinkIcon } alt="ícone de bebidas" />
       </Link>
 
       <Link
         to="/explore"
         data-testid="explore-bottom-btn"
+        src={ ExploreIcon }
       >
-        Explorar
+        <img src={ ExploreIcon } alt="ícone de explorar" />
       </Link>
 
       <Link
         to="/foods"
         data-testid="food-bottom-btn"
+        src={ MealIcon }
       >
-        Comidas
+        <img src={ MealIcon } alt="ícone de comidas" />
       </Link>
-    </div>
+    </footer>
   );
 }
 
